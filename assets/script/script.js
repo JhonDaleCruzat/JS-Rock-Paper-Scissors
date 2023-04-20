04b6b192838fb8e64f0ec7c28fbf6e7a2d79a7a7
JhonDaleCruzat <cruzat.jd@gmail.com>
2023-04-20 23:39:25 +0800

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,67 @@
+// Define Variables
+let buttons = document.querySelectorAll('button');
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
+const history = document.getElementById('history');
+let playerSelection, computerSelection;
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+// Function for Computer Choice
+function computerPlay() {
+    const choices = ['Conan', 'Ran', 'Detective Mouri'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+// Conditional Statement for Player Win / Computer Win
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+    return "It's a tie!";
+    } else if (playerSelection === 'Conan' && computerSelection === 'Detective Mouri' || playerSelection === 'Detective Mouri' && computerSelection === 'Ran' || playerSelection === 'Ran' && computerSelection === 'Conan') {
+        playerScoreCount++;
+        return `You win! You chose ${playerSelection} and Computer chose ${computerSelection}`;
+    } else {
+        computerScoreCount++;
+        return `You lose! You chose ${computerSelection} and Computer chose ${playerSelection}`;
+    }
+}
+
+// Function that updates the scoreboard after each match
+function updateScore() {
+    playerScore.textContent = playerScoreCount;
+    computerScore.textContent = computerScoreCount;
+}
+
+// Function that updates the match history using appendChild
+function updateHistory(result) {
+    const li = document.createElement('li');
+    li.textContent = result;
+    history.appendChild(li);
+    var lastLi = document.querySelector('#history li:last-child');
+	lastLi.scrollIntoView();
+}
+
+// Function that resets the game to 0
+function resetGame() {
+    playerScoreCount = 0;
+    computerScoreCount = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    history.innerHTML = '';
+}
+
+// Loops the score, if player/computer reached 5 points, alert will pop out to announce the winner then resets the game to 0
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = computerPlay();
+        const result = playRound(playerSelection, computerSelection);
+        updateScore();
+        updateHistory(result);
+        if (playerScoreCount === 5 || computerScoreCount === 5) {
+            alert(`Game over! ${playerScoreCount > computerScoreCount ? 'You win!' : 'You lose!'}`);
+            resetGame();
+        }
+    });
+});
